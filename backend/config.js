9d001bb780484c878a43cad74f60836f3be2a124
satcode8 <malyalasathwik@gmail.com>
2024-06-18 12:25:16 +0530

    first commit

---
+++ backend/config.js
@@ -0,0 +1,3 @@
+export const PORT = 5555;
+
+export const mongoDBURL = 'mongodb+srv://root:root@bookstore-mern.u86xtq6.mongodb.net/books-collection?retryWrites=true&w=majority&appName=bookstore-mern'
